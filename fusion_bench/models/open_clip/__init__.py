0709e307427855562283b169d1ad568baf2eeb98
tanganke <tang.anke@foxmail.com>
2025-04-21 22:12:05 +0800

    Add OpenCLIP model implementation and utilities
    - Introduced the OpenCLIP model with `ImageEncoder`, `ImageClassifier`, and `MultiHeadImageClassifier` classes for image processing and classification tasks.
    - Added utility functions for model saving and loading, as well as L1 norm computation between models.
    - Created a `variables_and_paths.py` file to manage model configurations and dataset paths.
    - Implemented helper functions in `utils.py` for various tasks including accuracy computation and parameter management.

---
+++ fusion_bench/models/open_clip/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains the support for the open_clip model.
+Modified from https://github.com/nik-dim/tall_masks/
+"""
+
+from .modeling import (
+    ImageClassifier,
+    ImageEncoder,
+    ImageClassifier,
+)
