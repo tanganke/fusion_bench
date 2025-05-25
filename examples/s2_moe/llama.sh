61045c94fe94c70d31320ce914da40f95fb366bf
loafei124 <18996341802@163.com>
2025-05-25 19:02:43 +0800

    feat(llama.sh): 添加CPU设备的s2_moe_llama_upscaling方法
    为了支持在CPU设备上运行s2_moe_llama_upscaling方法，添加了新的配置行。这将使测试和调试在无GPU环境下更加方便。

--- examples/s2_moe/llama.sh
+++ examples/s2_moe/llama.sh
@@ -1,2 +1,5 @@
 #######################llama3.2 3B task5#############################
 CUDA_VISIBLE_DEVICES=1 fusion_bench method=s2_moe/s2_moe_llama_upscaling method.device=cuda method.gate_k=8 method.k=8 modelpool=llama32_3b/llama-32-3B_TA8 taskpool=LMEvalHarnessTaskPool/lm_eval taskpool.tasks=[MergeBench/instruction_val,MergeBench/coding_val,MergeBench/math_val,MergeBench/multilingual_val,MergeBench/safety_val] report_save_path="outputs/llama32_3b/llama32_3b_TA5.json"
+
+
+fusion_bench method=s2_moe/s2_moe_llama_upscaling method.device=cpu method.gate_k=8 method.k=8 modelpool=CausalLMPool/mergebench/Llama-3.2-3B-Instruct taskpool=dummy
