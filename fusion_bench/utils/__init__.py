f7318856d56a53e84fba351f0c65a6468ea2dea4
Anke Tang <tang.anke@foxmail.com>
2025-06-13 03:18:44 +0800

    Enhance module imports and error handling
    - Added `LazyStateDict` import to `__init__.py` for better state management.
    - Updated `__init__.py` in `utils` to include `pylogger` for improved logging capabilities.
    - Modified `dtype.py` to include "half" as a valid dtype and improved error message for unsupported dtype strings.
    - Enhanced `seed_everything_by_time` function in `fabric.py` to handle optional `fabric` parameter and added logging for seed broadcasting.

--- fusion_bench/utils/__init__.py
+++ fusion_bench/utils/__init__.py
@@ -2,14 +2,14 @@
 import importlib
 from typing import Iterable
 
-from . import data, functools, path
+from . import data, functools, path, pylogger
 from .cache_utils import *
 from .devices import *
 from .dtype import parse_dtype
 from .fabric import seed_everything_by_time
 from .instantiate_utils import instantiate, is_instantiable
+from .lazy_state_dict import LazyStateDict
 from .misc import *
 from .packages import import_object
 from .parameters import *
 from .timer import timeit_context
-from .lazy_state_dict import LazyStateDict
