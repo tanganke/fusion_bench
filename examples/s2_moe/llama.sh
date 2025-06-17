738dbaf8655b8b55a33ef3ddc55428ea0d06da63
Loafei <18996341802@163.com>
2025-06-17 15:39:20 +0800

    fix: 更新模型保存路径和稀疏率配置
    修改模型保存路径从llama3.2-3B_S2MoE到llama3.2-8B_Instruct_S2MoE
    调整稀疏率从0.80增加到0.98
    添加安全评估脚本配置

--- examples/s2_moe/llama.sh
+++ examples/s2_moe/llama.sh
@@ -19,3 +19,7 @@ lm_eval --model_args pretrained="save_models/llama3.2-3B_S2MoE",dtype='bfloat16'
 
 #bigcode
 accelerate launch  main.py   --model /home/hfh/fusionbench_0517/save_models/llama3.2-3B_S2MoE --modeltype S2MoE   --max_length_generation 512   --precision bf16   --tasks humanevalplus,mbppplus   --temperature 0.2   --n_samples 10   --batch_size 10   --allow_code_execution   --metric_output_path outputs/code_eval.json   --use_auth_token
+
+
+#safety
+CUDA_VISIBLE_DEVICES=0,1,2,3 python evaluation/eval.py generators   --model_name_or_path MergeBench/Llama-3.2-3B-Instruct_safety   --model_input_template_path_or_name llama3   --tasks wildguardtest,harmbench,xstest,do_anything_now   --report_output_path outputs/safety_eval.json   --save_individual_results_path outputs/safety_generation.json   --batch_size 8
