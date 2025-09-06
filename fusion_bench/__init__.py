a35c2dd2e48b7dd12eff84ea6818e39f965c5da7
Anke Tang <tang.anke@foxmail.com>
2025-09-07 04:03:19 +0800

    feat: update import structure to use _available_algorithms and add extra_objects for method module

--- fusion_bench/__init__.py
+++ fusion_bench/__init__.py
@@ -12,6 +12,7 @@ from fusion_bench.utils.lazy_imports import LazyImporter
 
 from . import constants, metrics, optim, tasks
 from .constants import RuntimeConstants
+from .method import _available_algorithms
 
 _extra_objects = {
     "RuntimeConstants": RuntimeConstants,
@@ -22,7 +23,7 @@ _extra_objects = {
 }
 _import_structure = {
     "dataset": ["CLIPDataset"],
-    "method": ["BaseAlgorithm", "BaseModelFusionAlgorithm"],
+    "method": _available_algorithms,
     "mixins": [
         "CLIPClassificationMixin",
         "FabricTrainingMixin",
