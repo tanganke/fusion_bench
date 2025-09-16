80a870cce8f9788c24f89d8d5d9770066e1e9549
Anke Tang <tang.anke@foxmail.com>
2025-09-16 13:31:19 +0800

    feat: implement ResNet layer-wise and task-wise adamerging algorithms with configuration support

--- fusion_bench/method/adamerging/__init__.py
+++ fusion_bench/method/adamerging/__init__.py
@@ -1,6 +1,29 @@
 # flake8: noqa F401
-from .clip_layer_wise_adamerging import CLIPLayerWiseAdaMergingAlgorithm
-from .clip_task_wise_adamerging import CLIPTaskWiseAdaMergingAlgorithm
-from .flan_t5_layer_wise_adamerging import FlanT5LayerWiseAdaMergingAlgorithm
-from .gpt2_layer_wise_adamerging import GPT2LayerWiseAdaMergingAlgorithm
-from .llama_adamerging import LayerWiseAdaMergingForLlamaSFT
+import sys
+from typing import TYPE_CHECKING
+
+from fusion_bench.utils.lazy_imports import LazyImporter
+
+_import_structure = {
+    "clip_layer_wise_adamerging": ["CLIPLayerWiseAdaMergingAlgorithm"],
+    "clip_task_wise_adamerging": ["CLIPTaskWiseAdaMergingAlgorithm"],
+    "flan_t5_layer_wise_adamerging": ["FlanT5LayerWiseAdaMergingAlgorithm"],
+    "gpt2_layer_wise_adamerging": ["GPT2LayerWiseAdaMergingAlgorithm"],
+    "llama_adamerging": ["LayerWiseAdaMergingForLlamaSFT"],
+    "resnet_adamerging": ["ResNetLayerWiseAdamerging", "ResNetTaskWiseAdamerging"],
+}
+
+if TYPE_CHECKING:
+    from .clip_layer_wise_adamerging import CLIPLayerWiseAdaMergingAlgorithm
+    from .clip_task_wise_adamerging import CLIPTaskWiseAdaMergingAlgorithm
+    from .flan_t5_layer_wise_adamerging import FlanT5LayerWiseAdaMergingAlgorithm
+    from .gpt2_layer_wise_adamerging import GPT2LayerWiseAdaMergingAlgorithm
+    from .llama_adamerging import LayerWiseAdaMergingForLlamaSFT
+    from .resnet_adamerging import ResNetLayerWiseAdamerging, ResNetTaskWiseAdamerging
+
+else:
+    sys.modules[__name__] = LazyImporter(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+    )
--- fusion_bench/method/adamerging/utils.py
+++ fusion_bench/method/adamerging/utils.py
@@ -1,4 +1,9 @@
+from typing import List
+
 import torch
+import torch.nn as nn
+
+from fusion_bench.utils.type import TorchModelType
 
 
 def get_memory_usage(desc):
@@ -13,3 +18,56 @@ def get_memory_usage(desc):
     return (
         f"{desc}\nAllocated Memory: {allocated:.2f} MB\nCached Memory: {cached:.2f} MB"
     )
+
+
+@torch.no_grad()
+def construct_task_wise_merged_model(
+    pretrained_model: TorchModelType,
+    finetuned_models: List[TorchModelType],
+    clamp_weights: bool = False,
+    tie_weights: bool = True,
+    strict: bool = False,
+):
+    from fusion_bench.models.wrappers.task_wise_fusion import (
+        TaskWiseMergedModel,
+        get_task_wise_weights,
+    )
+
+    merging_weights = get_task_wise_weights(num_models=len(finetuned_models))
+    module = TaskWiseMergedModel(
+        task_wise_weight=merging_weights,
+        pretrained_model=pretrained_model,
+        finetuned_models=finetuned_models,
+        clamp_weights=clamp_weights,
+        tie_weights=tie_weights,
+        strict=strict,
+    )
+    return module
+
+
+@torch.no_grad()
+def construct_layer_wise_merged_model(
+    pretrained_model: TorchModelType,
+    finetuned_models: List[TorchModelType],
+    clamp_weights: bool = False,
+    tie_weights: bool = True,
+    strict: bool = False,
+):
+    from fusion_bench.models.wrappers.layer_wise_fusion import (
+        LayerWiseMergedModel,
+        get_layer_wise_weights,
+    )
+
+    merging_weights = get_layer_wise_weights(
+        num_models=len(finetuned_models),
+        num_layers=len([p for p in pretrained_model.parameters() if p.requires_grad]),
+    )
+    module = LayerWiseMergedModel(
+        layer_wise_weight=merging_weights,
+        pretrained_model=pretrained_model,
+        finetuned_models=finetuned_models,
+        clamp_weights=clamp_weights,
+        tie_weights=tie_weights,
+        strict=strict,
+    )
+    return module
