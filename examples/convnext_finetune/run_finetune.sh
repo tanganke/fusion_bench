38425a10f532228c94bd1de9440198cd7c3ee622
tanganke <tang.anke@foxmail.com>
2026-01-04 16:08:30 +0800

    Refactor finetuning script to improve GPU handling and remove unused variables

--- examples/convnext_finetune/run_finetune.sh
+++ examples/convnext_finetune/run_finetune.sh
@@ -1,38 +1,43 @@
 #! /usr/bin/env bash
+# if CUDA_VISIBLE_DEVICES is set, get the number of GPUs from it
+if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
+    NUM_GPUS=$(nvidia-smi -L | wc -l)
+else
+    NUM_GPUS=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
+fi
+echo "Using $NUM_GPUS GPUs"
+
 function finetune() {
     local output_dir="outputs/${model}/${dataset}"
-    local version="batch_size_${batch_size}_lr_${lr}_training_data_ratio_${training_data_ratio}"
+    local version="batch_size=${batch_size},lr=${lr}"
     if [ -d "$output_dir/$version" ]; then
         echo "$output_dir/$version exists, skip finetuning."
-        continue
+    else
+        # the per-GPU batch size is global batch size divided by number of GPUs
+        rich-run fusion_bench --config-name model_fusion \
+            path.log_dir=\"${output_dir}/$version\" seed=0 \
+            method=classification/image_classification_finetune \
+                method.max_epochs=-1 \
+                method.max_steps=4000 \
+                method.save_top_k=-1 \
+                method.save_interval=1000 \
+                method.save_on_train_epoch_end=false \
+                method.optimizer.lr=${lr} \
+                method.dataloader_kwargs.batch_size=$((${batch_size} / ${NUM_GPUS})) \
+            modelpool=ConvNextForImageClassification/${model} \
+            modelpool.models._pretrained_.dataset_name=${dataset} \
+            +dataset/image_classification/train@modelpool.train_datasets=${dataset} \
+            +dataset/image_classification/test@modelpool.val_datasets=${dataset}
     fi
-    rich-run fusion_bench --config-name model_fusion \
-        path.log_dir=${output_dir}/$version seed=0 \
-        method=classification/image_classification_finetune \
-            method.max_epochs=-1 \
-            method.max_steps=4000 \
-            method.save_top_k=-1 \
-            method.save_interval=1000 \
-            method.save_on_train_epoch_end=false \
-            method.training_data_ratio=${training_data_ratio} \
-            method.optimizer.lr=${lr} \
-            method.dataloader_kwargs.batch_size=${batch_size} \
-        modelpool=ConvNextForImageClassification/${model} \
-        modelpool.models._pretrained_.dataset_name=${dataset} \
-        +dataset/image_classification/train@modelpool.train_datasets=${dataset} \
-        +dataset/image_classification/test@modelpool.val_datasets=${dataset}
 }
 
-training_data_ratio=1.0
 for model in convnext-base-224; do
     for dataset in sun397 stanford-cars resisc45 eurosat svhn gtsrb mnist dtd oxford_flowers102 pcam fer2013 oxford-iiit-pet stl10 cifar100 cifar10 food101 fashion_mnist emnist_letters kmnist rendered-sst2; do
-        device_idx=0
+        # global batch size
         for batch_size in 64 128 256; do
             for lr in 0.005 0.001; do
-                CUDA_VISIBLE_DEVICES=${device_idx} finetune &
-                device_idx=$((device_idx + 1))
+                finetune
             done
         done
-        wait
     done
 done
